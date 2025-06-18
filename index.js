eb680647e77a5c299913fab774cc7f92dc3e0d9b
Михаил <st.mihaile87@gmail.com>
2025-06-18 06:01:14 +0400

    obj_1

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+1
+
+let undefindeVar;
+let nullVar = null;
+let emptyStringVar = '';
+let zeroVar = 0;
+let falseVar = false;
+
+console.log(undefindeVar ?? 'Результат');
+console.log(nullVar ?? 'Результат');
+console.log(emptyStringVar ?? 'Результат');
+console.log(zeroVar ?? 'Результат');
+console.log(falseVar ?? 'Результат');
+
+*/
+
+2
+
